b1d3c234f81675fcc748367efcbe609b24c81e98
TIAGO HENRIQUE ANGIOLETTI <tiago.angioletti@prof.sc.senac.br>
2025-03-05 10:52:40 -0300

    inicio

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(f(5));
+        }
+
+        static int f (int n)
+        {
+            
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n*f(n-1);
+        }
+    }
+}
